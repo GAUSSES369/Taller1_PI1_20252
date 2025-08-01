e8421e20aa077281045b071e483792e8aaad5431
GAUSSES369 <mavergara2@eafit.edu.co>
2025-08-01 11:26:52 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,8 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page<h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Moises Arturo Vergara Garces'})
+
+def about(request):
+    #return HttpResponse('<h1>Welcome to Home Page<h1>')
+    #return render(request, 'home.html')
+    return render(request, 'about.html', {'name':'About'})
