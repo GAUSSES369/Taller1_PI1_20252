2c0c644287f6b1ad11ea7c62490c36cf9c8def52
GAUSSES369 <mavergara2@eafit.edu.co>
2025-08-24 18:58:40 -0500

    newss

--- news/views.py
+++ news/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def news(request):
+    newss = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'newss':newss})
